11f905314514bfaa4ce6a5e43c82315ac63a69c2
JeffGaofei <soar_du@hotmail.com>
2015-11-16 20:36:45 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Setting up a list of 3 functions: get(data) setInv(erse Matrix) GetInv(erse Matrix)
+##Since X is a pre-defined x just as the numeric vector in the demo, the SET function is omitted
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()){
+  i <- NULL ##declare i to store the inversed matrix
+  get <- function() x ## get matrix x to be inversed
+  setInv <- function(y) i <<- y ##assign a value (the inversed matrix) to i
+  getInv <- function() i ##return the inversed matrix i
+  list(get=get,setInv=setInv,getInv=getInv)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getInv() ##read the cached data
+  if(!is.null(i)){ ##check whether there's data cached or not
+    message("getting cached data")
+    return(i) ##return cached data
+  }
+  ##If there's no data cached
+  data <- x$get() ## get matrix x to be inversed
+  i <- solve(data) ##solve the inverse and assign it to internal variable i
+  x$setInv(i) ##assign internal i to global i and store the data
+  i ##return global i
 }
